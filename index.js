42a24e1f51503b5836dadfdec4c6bafce1c21100
sendo-dev <endoushun.0928@gmail.com>
2022-07-11 08:44:06 +0900

    富澤版

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const line_config = {
     channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
     channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
 };
+const message_queue = ["こんにちは", "ご一緒に"];
+const reply_queue = ["これはこれは", "ホタテ"];
 
 // -----------------------------------------------------------------------------
 // Webサーバー設定
@@ -30,15 +32,20 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text"){
+        if (event.type == "message" && event.message.type == "text") {
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "これはこれは"
-                }));
+            let reply = "ちょっと何言ってるか分からない";
+            for (let i=0; i<message_queue.length; i++) {
+                if (event.message.text == message_queue[i]){
+                    reply = reply_queue[i];
+                    break;
+                }
             }
+            // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+            events_processed.push(bot.replyMessage(event.replyToken, {
+                type: "text",
+                text: reply
+            }));
         }
     });
 
