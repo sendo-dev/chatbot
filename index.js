02578582c4f6a97eb02d238f429daefb39e0212e
sendo-dev <endoushun.0928@gmail.com>
2022-07-13 21:30:08 +0900

    dialogflowの組み込み

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // モジュールのインポート
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const dialogflow = require("dialogflow");
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
@@ -21,6 +22,15 @@ server.listen(process.env.PORT || 3000);
 // APIコールのためのクライアントインスタンスを作成
 const bot = new line.Client(line_config);
 
+// Dialogflowのクライアントインスタンスを作成
+const session_client = new dialogflow.SessionsClient({
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    }
+});
+
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
@@ -33,20 +43,32 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text") {
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            let reply = unknown;
-            for (let i=0; i<messageQueue.length; i++) {
-                if (event.message.text == messageQueue[i]){
-                    reply = replyQueue[i];
-                    break;
-                }
-            }
-            // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-            events_processed.push(bot.replyMessage(event.replyToken, {
-                type: "text",
-                text: reply
-            }));
+        if (event.type == "message" && event.message.type == "text"){
+            events_processed.push(
+                session_client.detectIntent({
+                    session: session_client.sessionPath(process.env.GOOGLE_PROJECT_ID, event.source.userId),
+                    queryInput: {
+                        text: {
+                            text: event.message.text,
+                            languageCode: "ja",
+                        }
+                    }
+                }).then((responses) => {
+                    let message_text = unknown;
+                    // 「注文」関連のメッセージを受け取った場合
+                    if (responses[0].queryResult && responses[0].queryResult.action == "handle-order"){
+                        if (responses[0].queryResult.parameters.fields.menu.stringValue){
+                            message_text = `${responses[0].queryResult.parameters.fields.menu.stringValue}ですね。承りました。`;
+                        } else {
+                            message_text = `ご注文をどうぞ`;
+                        }
+                    }
+                    return bot.replyMessage(event.replyToken, {
+                        type: "text",
+                        text: message_text
+                    });
+                })
+            );
         }
     });
 
